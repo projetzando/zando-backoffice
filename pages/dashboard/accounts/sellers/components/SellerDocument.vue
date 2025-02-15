85ef06289b03f515d76bf7c34128cccb145d5ea1
Céleste NiLe'S GAKONO <87776997+Celesteniles@users.noreply.github.com>
2025-02-15 11:17:06 +0100

    seller details

---
+++ pages/dashboard/accounts/sellers/components/SellerDocument.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+defineProps<{
+    seller: Seller
+}>()
+
+</script>
+<template>
+    <div class="border rounded-lg px-4 pt-3">
+        <p class="flex flex-col space-y-1 mb-3">
+            <span class="font-semibold">Documents</span>
+        </p>
+    </div>
+</template>
---
+++ pages/dashboard/accounts/sellers/components/SellerHeader.vue
@@ -0,0 +1,26 @@
+<script setup lang='ts'>
+defineProps<{
+    seller: Seller
+}>()
+</script>
+
+<template>
+    <Card title="Nom" icon="bx:user">
+        <p> {{ seller?.company_name }}
+        </p>
+    </Card>
+
+    <Card title="Statut juridique" icon="heroicons:phone">
+        <p> {{ seller?.legal_status }}</p>
+    </Card>
+
+    <Card title="Siret" icon="material-symbols:type-specimen">
+        <p> {{ seller?.siret }}</p>
+    </Card>
+
+    <Card title="Numéro TVA" icon="hugeicons:job-link">
+        <p> {{ seller?.vat_number }}</p>
+    </Card>
+</template>
+
+<style lang="css" scoped></style>
